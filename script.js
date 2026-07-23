1644611ac6fcdc7c91d9befcc80377872ff1491c
angemon2 <poketeck@gmail.com>
2026-07-22 22:27:25 -0400

    Updated button message script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function myFun() {
             document.getElementById("demo")
-                .innerHTML = "I'm not sure why the first button doesn't work...";
+                .innerHTML = "The first button works!";
         }
