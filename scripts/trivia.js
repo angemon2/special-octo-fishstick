3961fe7059dbb0e7311b3ea1151d1976cbfe2a34
mehtaishita <ishitamehta33@outlook.com>
2026-07-28 20:03:29 -0500

    start game, display question

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -22,3 +22,76 @@ const questions = [
   }
   // add more here
 ]
+
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct; //set it to true
+    }
+    button.addEventListener('click', () => selectAnswer(button));
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function selectAnswer(selectedButton) {
+  Array.from(answerButtonsElement.children).forEach(button => {
+      button.disabled = true;
+      setStatusClass(button, button.dataset.correct);
+  });
+
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(selectedButton, correct);
+
+  // Delay revealing the "Next" button to allow users to review their choice
+  setTimeout(() => {
+      if (shuffledQuestions.length > currentQuestionIndex + 1) {
+          nextButton.classList.remove('hide');
+      }
+  }, 1000); // Adjust delay as needed
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+      element.classList.add('correct');
+  } else {
+      element.classList.add('wrong');
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
+}
+
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
